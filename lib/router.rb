236bba2f456becdafd0e32962cb4200115a932e0
Work <joshuablaine@users.noreply.github.com>
2016-06-07 15:43:57 -0700

    Router with basic http method routes

--- lib/router.rb
+++ lib/router.rb
@@ -18,6 +18,8 @@ class Route
   # use pattern to pull out route params (save for later?)
   # instantiate controller and call controller action
   def run(req, res)
+    route_params = {}
+    ControllerBase.new(req, res, {}).invoke_action(action_name)
   end
 end
 
@@ -25,27 +27,36 @@ class Router
   attr_reader :routes
 
   def initialize
+    @routes = []
   end
 
   # simply adds a new route to the list of routes
   def add_route(pattern, method, controller_class, action_name)
+    routes << Route.new(pattern, method, controller_class, action_name)
   end
 
   # evaluate the proc in the context of the instance
   # for syntactic sugar :)
   def draw(&proc)
+    self.instance_eval(&proc)
   end
 
   # make each of these methods that
   # when called add route
   [:get, :post, :put, :delete].each do |http_method|
+    define_method(http_method) do |pattern, controller_class, action_name|
+      add_route(http_method, pattern, controller_class, action_name)
+    end
   end
 
   # should return the route that matches this request
   def match(req)
+    routes.find { |route| route.matches?(req) }
   end
 
   # either throw 404 or call run on a matched route
   def run(req, res)
+    route = match(req)
+    route ? route.run(req, res) : res.status = "404 no matching route"
   end
 end
